efab6fcfe141c429bc96a499cbab60266226111e
YeganehNobakht <m.yeganeh.n1994@gamil.com>
2022-12-11 16:51:19 +0330

    feat: swagger ui

--- src/main/java/com/mktb/todolist/TodolistApplication.java
+++ src/main/java/com/mktb/todolist/TodolistApplication.java
@@ -2,14 +2,20 @@ package com.mktb.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class TodolistApplication {
+public class TodolistApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodolistApplication.class);
+	}
 }
--- src/main/java/com/mktb/todolist/configuration/SwaggerConfiguration.java
+++ src/main/java/com/mktb/todolist/configuration/SwaggerConfiguration.java
@@ -2,6 +2,11 @@ package com.mktb.todolist.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.math.BigDecimal;
 
@@ -10,13 +15,14 @@ import java.math.BigDecimal;
  * @created 11/12/2022 - 3:17 PM
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
 
 
         @Bean
         public Docket api() {
             return new Docket(DocumentationType.SWAGGER_2).select()
-                    .apis(RequestHandlerSelectors.basePackage("com.behsazan.didehBank.controller"))
+                    .apis(RequestHandlerSelectors.basePackage("com.mktb.todolist.web"))
                     .paths(PathSelectors.regex("/.*"))
                     .build().directModelSubstitute(BigDecimal.class, String.class);
         }
